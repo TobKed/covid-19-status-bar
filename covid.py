364d9bc1af0a9e404846f0d4f57e3a1ca094d51a
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-04-11 00:28:19 +0200

    Add event type menu choice

--- covid.py
+++ covid.py
@@ -60,20 +60,37 @@ class CountryMenuItem(rumps.MenuItem):
         self.country = country
 
 
+class EventTypeMenuItem(rumps.MenuItem):
+    def __init__(self, *args, **kwargs):
+        event_type = None
+        if "event_type" in kwargs:
+            event_type = kwargs.pop("event_type")
+
+        super().__init__(*args, **kwargs)
+        self.event_type = event_type
+
+
 class AwesomeStatusBarApp(rumps.App):
     APP_NAME = "Covid"
     DEFAULT_COUNTRY = Country("Poland", "poland", "PL")
+    DEFAULT_STATISTICS_EVENT_TYPE = StatisticsEventType.deaths
+    DEFAULT_STATISTICS_TIME_TYPE = StatisticsTimeType.total
     COUNTRIES_URL = "https://api.covid19api.com/countries"
 
     def __init__(self):
         super().__init__(self.APP_NAME)
         self._countries_list: Optional[List[Dict[str, str]]] = None
         self._selected_country: Country = self.DEFAULT_COUNTRY
+        self._selected_statistics_event_type = self.DEFAULT_STATISTICS_EVENT_TYPE
+        self._selected_statistics_time_type = self.DEFAULT_STATISTICS_TIME_TYPE
 
         self.menu_countries = rumps.MenuItem(title="Country")
         self.update_countries_selection()
 
-        self.menu = [self.menu_countries]
+        self.menu_event_types = rumps.MenuItem(title="Event type")
+        self.update_event_type_selection()
+
+        self.menu = [self.menu_event_types, self.menu_countries]
 
     @property
     def countries_list(self) -> List[Country]:
@@ -90,7 +107,7 @@ class AwesomeStatusBarApp(rumps.App):
                 continue
             menu_item = CountryMenuItem(
                 title="%s" % country.country,
-                callback=self.menu_item_callback,
+                callback=self.menu_country_item_callback,
                 country=country,
             )
             menu_item.state = 1 if country == self._selected_country else 0
@@ -109,29 +126,50 @@ class AwesomeStatusBarApp(rumps.App):
     def about(self, _) -> None:
         About().run()
 
-    def menu_item_callback(self, single_country_menu_item: CountryMenuItem) -> None:
+    def menu_country_item_callback(
+        self, single_country_menu_item: CountryMenuItem
+    ) -> None:
         self._selected_country = single_country_menu_item.country
         self.update_countries_selection()
         self.update_data()
 
     def update_data(self):
         statistics = self._selected_country.get_statistics()
-        deaths_today = statistics[StatisticsTimeType.today.name][
-            StatisticsEventType.deaths.name
+        deaths_today = statistics[self._selected_statistics_time_type.name][
+            self._selected_statistics_event_type.name
         ]
         self._update_title(
             value=deaths_today,
-            time_type=StatisticsTimeType.today.name,
-            event_type=StatisticsEventType.deaths.name,
+            time_type=self._selected_statistics_time_type.name,
+            event_type=self._selected_statistics_event_type.name,
         )
 
-    def _update_title(
-        self,
-        value: str,
-        time_type: Optional[str] = StatisticsTimeType.today.name,
-        event_type: Optional[str] = StatisticsEventType.deaths.name,
-    ) -> None:
+    def _update_title(self, value: str, time_type: str, event_type: str) -> None:
         self.title = f"{self.APP_NAME} - {event_type} {time_type} in {self._selected_country.country}: {value}"
+
+    def update_event_type_selection(self) -> None:
+        for _, event_type in StatisticsEventType.__members__.items():
+            if event_type.value in self.menu_event_types:
+                self.menu_event_types[event_type.value].state = (
+                    1 if event_type == self._selected_statistics_event_type else 0
+                )
+                continue
+            menu_item = EventTypeMenuItem(
+                title="%s" % event_type.value,
+                callback=self.menu_event_item_callback,
+                event_type=event_type,
+            )
+            menu_item.state = (
+                1 if event_type == self._selected_statistics_event_type else 0
+            )
+            self.menu_event_types.add(menu_item)
+
+    def menu_event_item_callback(
+        self, single_event_menu_item: EventTypeMenuItem
+    ) -> None:
+        self._selected_statistics_event_type = single_event_menu_item.event_type
+        self.update_event_type_selection()
+        self.update_data()
 
     @rumps.timer(600)
     def update(self, _) -> None:
